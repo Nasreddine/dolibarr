7955f1e11d1479842dbec380fe9b04b3df387883
chenina <nasredine.cheniki@ext.ec.europa.eu>
2025-05-23 10:57:25 +0100

    Docker run improvements for production environment

--- build/docker-prod/docker-run.sh
+++ build/docker-prod/docker-run.sh
@@ -23,6 +23,21 @@ fi
 echo "[docker-run] => Set Permission to www-data for /var/www/documents"
 chown -R www-data:www-data /var/www/documents
 
+# Create install.lock file for security
+echo "[docker-run] => Creating install.lock file if it doesn't exist"
+if [ ! -f /var/www/html/documents/install.lock ]; then
+    touch /var/www/html/documents/install.lock
+    chown www-data:www-data /var/www/html/documents/install.lock
+    chmod 400 /var/www/html/documents/install.lock
+fi
+
+# Secure conf.php file if it exists
+if [ -f /var/www/html/conf/conf.php ]; then
+    echo "[docker-run] => Setting proper permissions on conf.php"
+    chown www-data:www-data /var/www/html/conf/conf.php
+    chmod 400 /var/www/html/conf/conf.php
+fi
+
 echo "[docker-run] => update '${PHP_INI_DIR}/conf.d/dolibarr-php.ini'"
 cat <<EOF > "${PHP_INI_DIR}/conf.d/dolibarr-php.ini"
 date.timezone = ${PHP_INI_DATE_TIMEZONE:-UTC}
